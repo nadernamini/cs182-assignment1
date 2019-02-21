67fceee7104f091acce43dbcb3325a6ea0303424
Nader Namini <nadernamini@berkeley.edu>
2019-02-20 16:24:10 -0800

    removed regularization and hidden dimension hyperparameter search

--- run_cnn.py
+++ run_cnn.py
@@ -9,7 +9,6 @@ from deeplearning.solver import Solver
 
 def load_data():
     # Load the (preprocessed) CIFAR10 data.
-
     data = get_CIFAR10_data()
     for k, v in data.iteritems():
         print '%s: ' % k, v.shape
@@ -28,32 +27,27 @@ def run_cnns():
     best_acc, best_model = float('-inf'), None
     learning_rates = np.logspace(-4, -2, 8)
     weight_scales = np.logspace(-2, -1, 4)
-    regs = np.logspace(-6, -3, 4)
-    hidden_dims = np.arange(250, 1501, 250)
     for lr in learning_rates:
         for ws in weight_scales:
-            for reg in regs:
-                for hidden_dim in hidden_dims:
-                    # Train a really good model on CIFAR-10
-                    model = ConvNet(hidden_dims=(hidden_dim,), dropouts=(0.2, 0.3, 0.4), num_filters=(32, 64), N=2, M=1,
-                                    input_dim=(3, 32, 32), filter_size=3, num_classes=10, weight_scale=ws, reg=reg,
-                                    dtype=np.float32, seed=None, use_batchnorm=True)
+            # Train a really good model on CIFAR-10
+            model = ConvNet(hidden_dims=(500,), dropouts=(0.2, 0.3, 0.4), num_filters=(32, 64), N=2, M=1,
+                            input_dim=(3, 32, 32), filter_size=3, num_classes=10, weight_scale=ws,
+                            dtype=np.float32, seed=None, use_batchnorm=True)
 
-                    solver = Solver(model, small_data,
-                                    num_epochs=2, batch_size=200,
-                                    update_rule='adam',
-                                    optim_config={
-                                        'learning_rate': lr,
-                                    },
-                                    lr_decay=1 - 1e-6,
-                                    verbose=True, print_every=20)
-                    solver.train()
-                    if solver.best_val_acc > best_acc:
-                        print "******************************************************"
-                        best_model, best_acc = model, solver.best_val_acc
-                        print("best valid acc yet: %f   lr: %e ws: %e reg: %e hd: %e" % (
-                            best_acc, lr, ws, reg, hidden_dim))
-                        print "******************************************************"
+            solver = Solver(model, small_data,
+                            num_epochs=2, batch_size=200,
+                            update_rule='adam',
+                            optim_config={
+                                'learning_rate': lr,
+                            },
+                            lr_decay=1 - 1e-6,
+                            verbose=True, print_every=20)
+            solver.train()
+            if solver.best_val_acc > best_acc:
+                print "******************************************************"
+                best_model, best_acc = model, solver.best_val_acc
+                print("best valid acc yet: %f   lr: %e ws: %e" % (best_acc, lr, ws))
+                print "******************************************************"
 
 
 if __name__ == "__main__":
